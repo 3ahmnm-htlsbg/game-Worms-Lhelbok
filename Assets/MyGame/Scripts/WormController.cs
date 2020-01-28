f180e391c2984b454cd3d01873b2210e49afaa7d
LH03 <lea.helbok03@htl-salzburg.ac.at>
2020-01-28 15:08:38 +0100

    Add void update with key detector

--- Assets/MyGame/Scripts/WormController.cs
+++ Assets/MyGame/Scripts/WormController.cs
@@ -4,40 +4,11 @@ using UnityEngine;
 
 public class WormController : MonoBehaviour
 {
-    public int x;
-    public string text;
-
-
-    // Start is called before the first frame update
-    void Start()
+   void Update()
     {
-        /*
-        if (x > 0)
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            Debug.Log("x ist größer als null");
-        }
-        else
-        {
-            Debug.Log("x ist kleiner als null");
-        }
-
-        if (text == "ALE")
-        {
-            Debug.Log("In der text Variable steht ALE");
-        }
-        else
-        {
-            Debug.Log("In der text Variable steht  etwas anderes");
-        }
-        */
-
-        if (x > 10)
-        {
-            Debug.Log("x ist größer oder gleich 10");
-        }
-        else
-        {
-            Debug.Log("x ist kleiner oder gleich 10");
+            Debug.Log("Wird aufgerufen!");
         }
     }
 }
