d8259f89969e2ddb6878e01bb001f4d7547684c6
LH03 <lea.helbok03@htl-salzburg.ac.at>
2020-03-03 20:43:56 +0100

    add a health manager with a life counter, also add a place where one can regain one's lives

---
+++ Assets/MyGame/Scripts/Healthmanager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthmanager : MonoBehaviour
+{
+    public int health = 10;
+    public TextAlignment playerOneHealth;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "BulletShot" && health > 0)
+        {
+            health = health - 1;
+        }
+        if (collision.gameObject.tag == "PlusLife" && health > 10)
+        {
+            health = health + 1;
+        }
+    }
+}
